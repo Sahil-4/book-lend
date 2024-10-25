822f6c418d43d68e34189eb8a96e8954c6359b27
Sahil-4 <www.sahil.875020@gmail.com>
2024-10-25 17:26:02 +0530

    added request body validation

---
+++ backend/src/schema/validationSchema.ts
@@ -0,0 +1,65 @@
+import Joi from "joi";
+
+// User Validation Schema
+export const userSignupSchema = Joi.object({
+  name: Joi.string().min(3).optional(),
+  username: Joi.string().min(3).required(),
+  phone: Joi.string().min(10).required(),
+  password: Joi.string().min(6).required(),
+  bio: Joi.string().allow("", null),
+});
+
+export const userLoginSchema = Joi.object({
+  username: Joi.string().min(3),
+  phone: Joi.string().min(10),
+  password: Joi.string().min(6).required(),
+}).or("username", "phone");
+
+// Book Validation Schema
+export const bookSchema = Joi.object({
+  title: Joi.string().required(),
+  description: Joi.string().required(),
+  author: Joi.string().required(),
+  genre: Joi.string().required(),
+  preview: Joi.string().optional(),
+  rentingPrice: Joi.number(),
+  sellingPrice: Joi.number(),
+  status: Joi.string().required(),
+  sellerId: Joi.string().required(),
+}).or("sellingPrice", "rentingPrice");
+
+export const bookSchemaUpdate = Joi.object({
+  title: Joi.string().allow(null, ""),
+  description: Joi.string().allow(null, ""),
+  author: Joi.string().allow(null, ""),
+  genre: Joi.string().allow(null, ""),
+  preview: Joi.string().allow(null, ""),
+  rentingPrice: Joi.number().allow(null, ""),
+  sellingPrice: Joi.number().allow(null, ""),
+  status: Joi.string().allow(null, ""),
+  sellerId: Joi.string().allow(null, ""),
+});
+
+// Message Validation Schema
+export const messageSchema = Joi.object({
+  type: Joi.string().required(),
+  content: Joi.string().required(),
+  senderId: Joi.string().required(),
+  receiverId: Joi.string().required(),
+  chatId: Joi.string().required(),
+});
+
+// Transaction Validation Schema
+export const transactionSchema = Joi.object({
+  remark: Joi.string().optional(),
+  amount: Joi.string().required(),
+  sellerId: Joi.string().required(),
+  buyerId: Joi.string().required(),
+  bookId: Joi.string().required(),
+});
+
+// Notification Validation Schema
+export const notificationSchema = Joi.object({
+  type: Joi.string().required(),
+  content: Joi.string().required(),
+});
