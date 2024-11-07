0cdbe63142191455483c75f85beb6ff99325e778
Sahil-4 <www.sahil.875020@gmail.com>
2024-11-07 19:54:34 +0530

    integreted redis

---
+++ backend/src/services/redis.ts
@@ -0,0 +1,27 @@
+import Redis from "ioredis";
+import logger from "../utils/logger.js";
+
+const redis = new Redis({
+  host: process.env.REDIS_HOST,
+  port: parseInt(process.env.REDIS_PORT || "6379"),
+  password: process.env.REDIS_PASSWORD,
+  connectTimeout: 10000,
+  retryStrategy: (times) => Math.min(times * 50, 2000),
+});
+
+redis.on("connect", () => {
+  logger.info("redis connected");
+});
+
+redis.on("error", (err) => {
+  logger.error(err);
+});
+
+process.on("SIGINT", () => {
+  redis.quit(() => {
+    logger.info("redis connection closed");
+    process.exit(0);
+  });
+});
+
+export default redis;
