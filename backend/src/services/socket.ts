c53cac6b53c1caf70208a5e96ff8571ddd4c30c1
Sahil-4 <www.sahil.875020@gmail.com>
2024-11-07 20:44:13 +0530

    integreted socket io and redis

--- backend/src/services/socket.ts
+++ backend/src/services/socket.ts
@@ -1,6 +1,7 @@
 import { Server } from "socket.io";
 import { Server as HttpServer } from "http";
 import { authenticateIO } from "../middlewares/authenticate.js";
+import redis from "./redis.js";
 
 let io: Server | null = null;
 
@@ -15,39 +16,39 @@ const initiateSocketIO = (httpServer: HttpServer) => {
   io.use(authenticateIO);
 
   // handle session
-  io.on("connection", (socket) => {
-    console.log(`User connected`);
-    console.log(`Socket id: ${socket.id}`);
-    console.log(`User id: ${socket.data.user_id}`);
-
-    // !TODO - add user in redis - user id to socket id mapping
-    // { userid -> socketid }
+  io.on("connection", async (socket) => {
+    // add user in redis - user id to socket id mapping
+    await redis.set(`user:${socket.data.user_id}:socket`, socket.id);
 
     // acknowledgement - user connected
     socket.emit("connected", "Connected to the Socket.IO server");
 
-    socket.on("join-chat", (data) => {
-      // !TODO - join user a room with chat id
-      // add user id in list mapped with chat id in redis
-      // { chatId - > { user1 id, user2 id } }
-      // if chatid map does not exists create it
+    // join room with chatId
+    socket.on("join-chat", async (data) => {
+      const { chatId } = data;
+
+      socket.join(chatId);
+      // add user and room details in redis
+      await redis.sadd(`chat:${chatId}:users`, socket.data.user_id);
     });
 
-    socket.on("leave-chat", (data) => {
-      // !TODO - un-join user a room with chat id
-      // remove user id from list mapped with chat id in redis
-      // chatId - > { user1 id, user2 id }
-      // if chatid map contains 0 elements/users delete it from redis
+    // leave room with chatId
+    socket.on("leave-chat", async (data) => {
+      const { chatId } = data;
+
+      socket.leave(chatId);
+      await redis.srem(`chat:${chatId}:users`, socket.data.user_id);
+
+      // delete chatId room info if there are no users
+      const remainingUsers = await redis.scard(`chat:${chatId}:users`);
+      if (remainingUsers === 0) {
+        await redis.del(`chat:${chatId}:users`);
+      }
     });
 
-    // !TODO - add this in add new message controller
-    // send message to user id or in chat id room
-    // io.to(user).emit("message-receive", {});
-
-    socket.on("disconnect", () => {
-      console.log(`User disconnected: ${socket.id}`);
-      // !TODO - remove user from redis - user id to socket id mapping
-      // { userid -> socketid } delete it completely
+    // delete user from redis
+    socket.on("disconnect", async () => {
+      await redis.del(`user:${socket.data.user_id}:socket`);
     });
   });
 };
