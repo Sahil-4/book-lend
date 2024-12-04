c54479d659f845d80a1ee03725a5e7487de87025
Sahil-4 <www.sahil.875020@gmail.com>
2024-12-04 19:32:01 +0530

    added icons

---
+++ frontend/src/components/common/icons.tsx
@@ -0,0 +1,8 @@
+export {
+  MdMenu as MenuIcon,
+  MdDelete as DeleteIcon,
+  MdClose as CloseIcon,
+  MdSearch as SearchIcon,
+  MdEdit as EditIcon,
+  MdSend as SendIcon,
+} from "react-icons/md";
