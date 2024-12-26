7296c4ff6fc0f4972e4de51bcf0c0ffce3f5aff3
Sahil-4 <www.sahil.875020@gmail.com>
2024-12-26 20:06:28 +0530

    add: next/image remotePatterns

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        hostname: "res.cloudinary.com",
+        protocol: "http",
+        port: "",
+        search: "",
+        pathname: "/book-lend-user-assets/image/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
