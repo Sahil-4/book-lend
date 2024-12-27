7a9d2330381158090a51b3d8d4698c4d498e2fd3
Sahil-4 <www.sahil.875020@gmail.com>
2024-12-27 20:42:09 +0530

    add: added function to make api call to get new auth token

--- frontend/src/api/auth.ts
+++ frontend/src/api/auth.ts
@@ -30,6 +30,15 @@ export const logout = async () => {
   }
 };
 
+export const issueAuthToken = async () => {
+  try {
+    const response = await API.get("/api/v1/users/authtoken");
+    return response.data as ResponseType;
+  } catch (error) {
+    handleApiError(error);
+  }
+};
+
 export const getUserProfile = async () => {
   try {
     const response = await API.get("/api/v1/users/profile");
