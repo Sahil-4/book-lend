5acf9edc9b748dbe4a28221dc8a0780118930307
Sahil-4 <www.sahil.875020@gmail.com>
2025-01-06 00:54:12 +0530

    update: updated edit profile form to preview avatar before submitting

--- frontend/src/app/profile/edit/page.tsx
+++ frontend/src/app/profile/edit/page.tsx
@@ -1,16 +1,23 @@
 "use client";
 
+import Image from "next/image";
 import { useRouter } from "next/navigation";
-import { FormEvent } from "react";
+import { ChangeEvent, FormEvent, useState } from "react";
 import { useAppDispatch, useAppSelector } from "@/lib/hooks";
 import { updateUserProfile } from "@/lib/features/auth/authSlice";
 import { UserT } from "@/types/user";
 import styles from "@/styles/pages/edit-profile.module.css";
 
 const Page = () => {
+  const [avatar, setAvatar] = useState<string>("");
   const user: UserT | null = useAppSelector((state) => state.auth.user);
   const dispatch = useAppDispatch();
   const router = useRouter();
+
+  const updateAvatarURI = (e: ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files ? e.target.files[0] : null;
+    if (file) setAvatar(URL.createObjectURL(file));
+  };
 
   const handleFormSubmit = (e: FormEvent) => {
     e.preventDefault();
@@ -26,8 +33,16 @@ const Page = () => {
         <h1>Edit Profile</h1>
         <form onSubmit={handleFormSubmit} className={styles.edit_profile_form}>
           <div className={styles.edit_profile_form_left}>
-            <input type="file" name="avatar" id="avatar" accept="image/*" />
-            <label htmlFor="avatar">Avatar</label>
+            <input
+              type="file"
+              name="avatar"
+              id="avatar"
+              accept="image/*"
+              onChange={updateAvatarURI}
+            />
+            <label htmlFor="avatar">
+              {avatar ? <Image src={avatar} alt={avatar} fill={true} /> : "Avatar"}
+            </label>
           </div>
 
           <div className={styles.edit_profile_form_right}>
