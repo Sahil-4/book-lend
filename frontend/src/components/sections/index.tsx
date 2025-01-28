e931e513dcabccc2c5b99140e994a35675eb1049
Sahil-4 <www.sahil.875020@gmail.com>
2025-01-29 01:29:20 +0530

    add: infinite scroll hook and list component

--- frontend/src/components/sections/index.tsx
+++ frontend/src/components/sections/index.tsx
@@ -1,4 +1,5 @@
 import Featured from "@/components/sections/featured";
 import RecentlyAddedBooks from "@/components/sections/recently-added-books";
+import List from "@/components/sections/list";
 
-export { Featured, RecentlyAddedBooks };
+export { Featured, RecentlyAddedBooks, List };
