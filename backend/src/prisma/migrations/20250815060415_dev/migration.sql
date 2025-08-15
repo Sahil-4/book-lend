f4213326aedc5138a8da1cfeab6be7cca8009c39
Sahil-4 <www.sahil.875020@gmail.com>
2025-08-15 11:35:40 +0530

    chore(db): prisma migrate

---
+++ backend/src/prisma/migrations/20250815060415_dev/migration.sql
@@ -0,0 +1,33 @@
+-- DropForeignKey
+ALTER TABLE "Book" DROP CONSTRAINT "Book_sellerId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Message" DROP CONSTRAINT "Message_chatId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Message" DROP CONSTRAINT "Message_receiverId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Message" DROP CONSTRAINT "Message_senderId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Notification" DROP CONSTRAINT "Notification_userId_fkey";
+
+-- AlterTable
+ALTER TABLE "Message" ALTER COLUMN "senderId" DROP NOT NULL,
+ALTER COLUMN "receiverId" DROP NOT NULL;
+
+-- AddForeignKey
+ALTER TABLE "Book" ADD CONSTRAINT "Book_sellerId_fkey" FOREIGN KEY ("sellerId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Notification" ADD CONSTRAINT "Notification_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Message" ADD CONSTRAINT "Message_senderId_fkey" FOREIGN KEY ("senderId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Message" ADD CONSTRAINT "Message_receiverId_fkey" FOREIGN KEY ("receiverId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Message" ADD CONSTRAINT "Message_chatId_fkey" FOREIGN KEY ("chatId") REFERENCES "Chat"("id") ON DELETE CASCADE ON UPDATE CASCADE;
